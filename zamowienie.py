741741479656cd6c5583a35c7e2b81f495d901dc
Karol Przybycin <przybycinkarol@gmail.com>
2026-02-14 18:57:47 +0100

    Wstępnie stworzona klasa zamówienia

---
+++ zamowienie.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from klient import Klient
+
+class Zamowienie:
+    def __init__(self,klient,koszyk = []):
+        self.klient = klient
+        self.koszyk = koszyk
+        self.data = datetime.now(ZoneInfo("Europe/Warsaw"))
+
+    def dodaj_produkt(self,produkt):
+        self.produkt = produkt
+        if produkt.czy_tylko_dla_pelnoletnich == True:
+            if self.klient.czy_pelnoletni() == True:
+                self.koszyk.append(produkt)
+        else:
+            self.koszyk.append(produkt)
+
+
+
+
+
+
